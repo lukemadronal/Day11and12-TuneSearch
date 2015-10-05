939eac0035d64862b7516dadfa3c0b73f2bd0c83
Luke Madronal <madronal@lafayette.edu>
2015-10-05 13:11:53 -0400

    initial commit for iTune

---
+++ iTune/iTune/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iTune
+//
+//  Created by Luke Madronal on 10/5/15.
+//  Copyright © 2015 Luke Madronal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
